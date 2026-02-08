46507704755e0fb9836fd3ee21f9ecaa1ebd378b
ChimichangaKid <realjaycegatsby@gmail.com>
2026-02-07 16:22:11 -0800

    initial webpage testing for github deployment

---
+++ app/garsdle.js
@@ -0,0 +1,70 @@
+
+
+var number_of_guesses = 5;
+var game_done = false;
+var win = false;
+var current_guess = 0;
+
+const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+const slider = document.getElementById("time_slider");
+const slider_display = document.getElementById("slider_value");
+const hint = document.getElementById("hint_label");
+const solution = document.getElementById("solution");
+const num_guesses_label = document.getElementById("guesses_label");
+
+
+
+window.onload = function () {
+    slider.addEventListener("input", () => {
+        slider_display.textContent = sliderToDate(slider.value);
+    });
+
+    initialize()
+};
+
+function initialize() {
+    document.getElementById("guess_button").addEventListener("click", checkAnswer);
+    solution.innerText = "";
+}
+
+function checkAnswer() {  
+    fetch("./downloads/daily_info.json").then(response => response.json()).then(data => {
+        todays_data = data.upload_date_int
+    
+        if (current_guess >= number_of_guesses) {
+            return;
+        }
+
+        var guessed_value = slider.value;   
+
+        if (guessed_value == todays_data) {
+            solution.innerText = "You Win!";
+            win = true;
+        }
+
+        if (guessed_value > todays_data) {
+            hint.textContent = "You have guessed too high.";
+        } else if (guessed_value < todays_data) {
+            hint.textContent = "You have guessed too low.";
+        }
+
+        current_guess += 1;
+
+        num_guesses_label.innerText = `Number of Guesses Left = ${number_of_guesses - current_guess}`;
+
+        if(current_guess >= number_of_guesses){
+            solution.innerText = "You Lose.";
+        }
+    });
+
+    
+}
+
+
+
+function sliderToDate(value, startYear = 2023) {
+    const monthIndex = (value - 1) % 12;
+    const yearOffset = Math.floor((value - 1) / 12);
+    return `${months[monthIndex]} ${startYear + yearOffset}`;
+}
--- src/downloader/video_downloader.py
+++ src/downloader/video_downloader.py
@@ -1,11 +1,14 @@
 import logging
 import random
 import yt_dlp
+import json
+import os
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 youtube_channel = "https://www.youtube.com/@TheGarschive/shorts"
+data_dir = r"downloads\daily_info.json"
 
 class YouTubeDownloaderHelper:
 
@@ -14,9 +17,12 @@ class YouTubeDownloaderHelper:
     }
 
     download_opts = {
-        "format": "bestvideo+bestaudio/best",
-        "merge_output_format": "mp4",      
-        "outtmpl": "downloads/%(title)s.%(ext)s",
+        "format": "bv*+ba/b",
+        "merge_output_format": "mp4",
+        "outtmpl": "downloads/daily.mp4",
+        "force_overwrites": True,
+        "nopart": True,
+        "continuedl": False,
     }
 
     def __init__(self, channel=youtube_channel, debug=False):
@@ -45,17 +51,35 @@ class YouTubeDownloaderHelper:
         
         with yt_dlp.YoutubeDL(self.info_opts) as ydl:
             video_info = ydl.extract_info(self._selected_video, download=False)
-        
-        self._video_info["upload_date"] = video_info["upload_date"]
+
+        date = video_info["upload_date"]
+
+        self._video_info["upload_date"] = date
+        self.convert_date_to_int(date)
+
+        with open(data_dir, 'w', encoding="utf-8") as f:
+            json.dump(self._video_info, f, ensure_ascii=False, indent=4)
 
     def download_video(self) -> None:
         if self._selected_video == "":
             logger.warning("No video has been selected.")
             return
+
+        if os.path.exists("downloads/daily_video.mp4"):
+            os.remove("downloads/daily_video.mp4")
         
         with yt_dlp.YoutubeDL(self.download_opts) as ydl:
             ydl.download([self._selected_video])
-        
+
+            os.rename("downloads/daily.mp4", "downloads/daily_video.mp4")
+
+    def convert_date_to_int(self, value: str):
+        year = int(value[:4])
+        month = int(value[4:6])
+
+        print(f"year {year}, month {month}")
+
+        self._video_info["upload_date_int"] = 12 * (year - 2023) + month
 
 if __name__ == "__main__":
     ytdownloader = YouTubeDownloaderHelper(debug=True)
